9b8d3dad147612dc47c57e4d60f32e24826884a6
hardik355 <kapspatel355@gmail.com>
2019-07-18 11:40:39 +0530

    Ransack gem Search and Filter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :products do
+    collection { post :search, to: 'products#index' }
+  end
+  
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'products#index'
 end
