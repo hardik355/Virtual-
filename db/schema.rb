fc55a24a6ec8642f0f460f995f7bdc4f680636fb
hardik355 <kapspatel355@gmail.com>
2020-01-02 10:05:36 +0530

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_091429) do
+ActiveRecord::Schema.define(version: 2019_07_18_104724) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
